7278267287d72fd3192d294c4d34e4c2e17a7ec1
Amer <amer.krlicbegovic@gmail.com>
2018-08-20 12:54:35 +0200

    Compiling and migration smart contracts

--- contract/contracts/ApprovalContract.sol
+++ contract/contracts/ApprovalContract.sol
@@ -4,19 +4,19 @@ contract ApprovalContract {
 
   address public sender;
   address public receiver;
-  address constant public approver = ;
+  address constant public approver = 0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef;
 
-  function ApprovalContract() public {
+  function constructor() public {
     // tbd
   }
 
   function deposit(address _receiver) external payable {
     require(msg.value > 0);
     sender = msg.sender;
-    receiver = _receiver);
+    receiver = _receiver;
   }
 
-  funtion viewApprover() external pure returns(address) {
+  function viewApprover() external pure returns(address) {
     return(approver);
   }
 
